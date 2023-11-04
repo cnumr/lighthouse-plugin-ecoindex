e1f22e2989144b628983a814f2cbbb9171bf3849
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-11-04 07:31:33 +0000

    feat: better puppeteer script

--- tests/lhci/.puppeteerrc.js
+++ tests/lhci/.puppeteerrc.js
@@ -10,12 +10,22 @@ module.exports = async (browser, context) => {
   const session = await page.target().createCDPSession()
   await page.goto(url, { waitUntil: 'networkidle0' })
   await new Promise(r => setTimeout(r, 3 * 1000))
+  const dimensions = await page.evaluate(() => {
+    var body = document.body, html = document.documentElement;
+
+    var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    return {
+      width: document.documentElement.clientWidth,
+      height: height,
+      deviceScaleFactor: window.devicePixelRatio,
+    }
+  })
   // We need the ability to scroll like a user. There's not a direct puppeteer function for this, but we can use the DevTools Protocol and issue a Input.synthesizeScrollGesture event, which has convenient parameters like repetitions and delay to somewhat simulate a more natural scrolling gesture.
   // https://chromedevtools.github.io/devtools-protocol/tot/Input/#method-synthesizeScrollGesture
   await session.send('Input.synthesizeScrollGesture', {
     x: 100,
     y: 600,
-    yDistance: -2500,
+    yDistance: -dimensions.height,
     speed: 1000,
   })
   // await page.click('#didomi-notice-agree-button');
