1d714d5450fb7f63826b595af04bde10ef54cd62
Renaud Heluin <renaud.heluin@novagaia.fr>
2023-12-11 12:02:22 +0000

    feat: randomize userAgent

--- lighthouse-plugin-ecoindex/cli/commands.js
+++ lighthouse-plugin-ecoindex/cli/commands.js
@@ -165,7 +165,7 @@ function getLighthouseConfig(
           width: 1920,
           height: 1080,
         },
-        emulatedUserAgent: constants.userAgents.desktop,
+        emulatedUserAgent: userAgentStrings[Math.floor(Math.random() * userAgentStrings.length)],
         maxWaitForLoad: 60 * 1000,
         disableStorageReset: isWarm,
       },
@@ -173,6 +173,13 @@ function getLighthouseConfig(
     },
   }
 }
+
+const userAgentStrings = [
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.2227.0 Safari/537.36',
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.3497.92 Safari/537.36',
+  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+];
 
 const getPuppeteerConfig = {
   headless: 'new',
