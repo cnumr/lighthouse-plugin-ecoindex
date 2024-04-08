a7cc2ce9a759ba84313e3f2527c7186c17177c06
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-04-08 10:50:43 +0000

    feat: rm exemples local, use link instead !

--- examples/simple-text-local/dom-informations.cjs
+++
@@ -1,5 +0,0 @@
-const DOMInformations = async function DOMInformations() {
-  const { default: DOMInformations } = await import('./dom-informations.js')
-  return DOMInformations()
-}
-module.exports = DOMInformations
