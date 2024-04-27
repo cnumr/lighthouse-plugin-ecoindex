0a2e05438b36b35ad623c30dc0800f900e0e0bb4
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-04-27 09:55:19 +0000

    feat: working with DOMInformation, custom gatherer

---
+++ examples/lhci-linked-lib/custom-config.js
@@ -0,0 +1,38 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const DOMInformations = import('lighthouse-plugin-ecoindex/gatherers/dom-informations.js')
+/** @type {LH.Config} */
+module.exports = {
+  formFactor: 'desktop',
+  // https://github.com/GoogleChrome/lighthouse/blob/main/core/config/constants.js
+  throttling: {
+    rttMs: 40,
+    throughputKbps: 10 * 1024,
+    cpuSlowdownMultiplier: 1,
+    requestLatencyMs: 0, // 0 means unset
+    downloadThroughputKbps: 0,
+    uploadThroughputKbps: 0,
+  },
+  screenEmulation: {
+    mobile: false,
+    width: 1920,
+    height: 1080,
+  },
+  emulatedUserAgent: 'desktop',
+  maxWaitForLoad: 60 * 1000,
+  plugins: ['lighthouse-plugin-ecoindex'],
+  disableStorageReset: true,
+  preset: 'desktop',
+  extends: "lighthouse:default",
+  artifacts: [
+    {
+      id: 'DOMInformations', 
+      // must adapte the path to the dom-informations.js file, it must be an absolute path
+      gatherer: '/workspace/lighthouse-plugin-ecoindex/gatherers/dom-informations.js'
+    },
+  ],
+}
