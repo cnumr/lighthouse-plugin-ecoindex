8295b45bbe8c9c1ddf852bcf16b7babbd4fe9b95
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-04-28 23:30:32 +0200

    feat: configi for lhci

--- .devcontainer/simpleContainer/updateContentCommand/updateContentCommand.sh
+++ .devcontainer/simpleContainer/updateContentCommand/updateContentCommand.sh
@@ -4,20 +4,16 @@ npm install -g @lhci/cli@0.12.0
 echo "install lighthouse"
 npm install -g lighthouse
 echo "install puppeteer browsers"
-npx puppeteer browsers install chrome@121.0.6167.85
-echo "install puppeteer"
-npm install -g puppeteer
+npx puppeteer browsers install chrome@121.0.6167.85 -y
 echo "chmod 666 /var/run/docker.sock"
 sudo chmod 666 /var/run/docker.sock
 cd /workspace/lighthouse-plugin-ecoindex && npm link
+echo "install http-server"
+npm install --global http-server
+echo "install retypeapp"
+npm install retypeapp --global
+echo "WARN: retype is not working in devcontainer!"
 echo "updateContentCommand ENDED 🎉"
 
-echo "Chrome installation STARTING ⏳"
-apt-get update --fix-missing && apt-get -y upgrade && apt-get install -y git wget gnupg && apt-get clean
-
-wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt-get update
-apt-get install google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 -y --no-install-recommends
-rm -rf /var/lib/apt/lists/*
-echo "Chrome installation ENDED 🎉"
+sudo sh /workspace/.devcontainer/simpleContainer/updateContentCommand/install-chrome.sh
+sudo sh /workspace/.devcontainer/simpleContainer/updateContentCommand/install-git-lfs.sh
