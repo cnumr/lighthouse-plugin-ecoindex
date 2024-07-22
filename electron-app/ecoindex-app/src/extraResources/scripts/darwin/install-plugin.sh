04eaf9a994850e77d1eac56ba47f162032c867da
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-07-22 14:26:09 +0200

    feat: reactivate the puppeteer installation

--- electron-app/ecoindex-app/src/extraResources/scripts/darwin/install-plugin.sh
+++ electron-app/ecoindex-app/src/extraResources/scripts/darwin/install-plugin.sh
@@ -4,10 +4,11 @@ echo "Install plugin launched 🚀"
 unset npm_config_prefix > /dev/null 2>&1
 source ~/.zshrc > /dev/null 2>&1
 
-echo "which npm : $(which npm)"
 echo "which node : $(which node)"
-# npx puppeteer browsers install chrome@121.0.6167.85
-echo "npm install -g lighthouse-plugin-ecoindex"
+echo "which npm : $(which npm)"
+echo "Installation of Puppetter/Chrome Browser v121.0.6167.85"
+npx puppeteer browsers install chrome@121.0.6167.85 > /dev/null 2>&1
+echo "Installation on the plugin Lighthouse-ecoindex"
 npm install -g lighthouse-plugin-ecoindex > /dev/null 2>&1
 
 echo "Install puppeteer browser and plugin done. 🎉"
