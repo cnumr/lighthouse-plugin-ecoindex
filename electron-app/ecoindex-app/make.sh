24ab1856657c43c62cc24ecc31128a2381a80e69
Renaud Héluin <renaud.heluin@novagaia.fr>
2024-08-01 13:59:58 +0200

    chore: add logs to make file

--- electron-app/ecoindex-app/make.sh
+++ electron-app/ecoindex-app/make.sh
@@ -1,4 +1,9 @@
+echo "make is starting 🧩"
 rm -rf out/* --y
+echo "make -- --arch=x64 --platform=win32"
 npm run make -- --arch=x64 --platform=win32
+echo "make -- --arch=arm64 --platform=win32"
 npm run make -- --arch=arm64 --platform=win32
+echo "make -- --arch=arm64 --plateform=darwin"
 npm run make -- --arch=arm64 --plateform=darwin
+echo "Make ar done 🚀"
