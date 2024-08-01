7793d963c964d184c7852a7227dc284fc9fe7a0f
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-08-02 00:15:15 +0200

    chore: disabled win32 release

--- electron-app/ecoindex-app/make.sh
+++ electron-app/ecoindex-app/make.sh
@@ -1,9 +1,12 @@
 echo "make is starting 🧩"
-rm -rf out/* --y
-echo "make -- --arch=x64 --platform=win32"
-npm run make -- --arch=x64 --platform=win32
-echo "make -- --arch=arm64 --platform=win32"
-npm run make -- --arch=arm64 --platform=win32
+echo "clean out folder"
+npm run clean:out
+# echo "make -- --arch=x64 --platform=win32"
+# npm run make:win32:x64
+# echo "make -- --arch=arm64 --platform=win32"
+# npm run make:win32:arm64
 echo "make -- --arch=arm64 --plateform=darwin"
-npm run make -- --arch=arm64 --plateform=darwin
+npm run make:darwin:arm64
+echo "make -- --arch=x64 --plateform=darwin"
+npm run make:darwin:x64
 echo "Make ar done 🚀"
