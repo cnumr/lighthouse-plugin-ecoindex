458d8e9868793f9db209ce276bd41d88305b43a5
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-08-18 18:49:37 +0200

    chore: comment

--- electron-app/ecoindex-app/src/main/handlers/IsNodeInstalledOnHost.ts
+++ electron-app/ecoindex-app/src/main/handlers/IsNodeInstalledOnHost.ts
@@ -2,7 +2,12 @@ import { IpcMainEvent, IpcMainInvokeEvent } from 'electron'
 
 import { getMainLog } from '../main'
 
-export const isNodeInstalled = (
+/**
+ *
+ * @param _event
+ * @deprecated see HandleGetNodeDir_NpmDir.ts handleNodeInstalled
+ */
+const isNodeInstalled = (
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _event?: IpcMainEvent | IpcMainInvokeEvent
 ) => {
