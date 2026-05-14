b3af93de5a564d4c0be85298027656ac861c5fc4
Renaud heluin <renaud.heluin@novagaia.fr>
2026-05-14 14:53:14 +0200

    feat(i18n): migrate warn-nodes-count displayValue to str_()

--- libs/ecoindex-lh-plugin-ts/src/audits/warn-nodes-count.ts
+++ libs/ecoindex-lh-plugin-ts/src/audits/warn-nodes-count.ts
@@ -13,6 +13,7 @@ const UIStrings = {
     'Information ⚠️ : Ecoindex nodes number might be ≠ Lighthouse nodes number.',
   description:
     "Green IT would like web service designers to favor the use of SVGs over bitmap images (PNG, GIF, JPEG), which weigh down the page, whenever possible. To encourage this practice, Green IT has adapted its DOM calculation method, which explains why the results of Lighthouse and Ecoindex calculations are sometimes different. 1st difference: In order to maximize the use of SVG, which impacts the DOM, Green IT's calculation method excludes the sub-elements of an SVG tag from the calculation. Lighthouse, on the other hand, does not exclude any element ⇒ DOM.Lighthouse > DOM.Ecoindex. 2nd difference: Lighthouse performs its calculation when the page is rendered, but does not take into account elements added or modified in JavaScript, whereas the Ecoindex calculation engine does ⇒ DOM.Lighthouse < DOM.Ecoindex.  [Learn more about the Ecoindex, Analysis methodology](https://www.ecoindex.fr/comment-ca-marche/#m%C3%A9thodologie-danalyse)",
+  displayValue: 'Ecoindex nodes: {count}',
 }
 const str_ = createIcuMessageFn(import.meta.url, UIStrings)
 
@@ -37,7 +38,7 @@ class WarnNodesCount extends Audit {
     // We can only display the Ecoindex value as an informational warning
     return {
       score: 1, // Always pass, this is just an informational warning
-      displayValue: `Ecoindex nodes: ${value}`,
+      displayValue: str_(UIStrings.displayValue, { count: value }),
       numericValue: value,
       numericUnit: 'DOM elements' as 'element',
     }
